6eedea1fb3a19c65e7971a0e95d6ea8db7a25f88
Aafiya choudhary <119157685+Aafiya-01@users.noreply.github.com>
2024-06-21 18:38:40 +0530

    Add files via upload

---
+++ lect47.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void moveAllneg(vector<int>&arr, int n){
+    //dutch national flag algo 
+int l=0, h=n-1;
+while(l<=h){
+    if(arr[l]<0){
+        l++;}
+    else if(arr[h]>0){
+        h--;       
+    }
+    else{
+        swap(arr[l],arr[h]);  
+        l++;
+        h--;
+    }
+  }  
+}
+
+int main(){
+  vector<int>arr={1,2,-3,4,-5,6};
+  int n = arr.size();
+
+  moveAllneg(arr, n);   
+  cout<<"sorted array is: "<<endl; 
+  for(int i=0; i<n; i++){
+    cout<<arr[i]<<" ";
+  }
+  cout<<endl;
